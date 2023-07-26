8745955689118b2bcc5fbc97dd3bd20737a90571
Emmanuel Oloyede <emmanueltopea@gmail.com>
2023-07-27 00:00:51 +0100

    feat: add logger

---
+++ src/logger.js
@@ -0,0 +1,47 @@
+const winston = require("winston")
+
+const customLoggingLevels = {
+    levels: {
+        warn: 0,
+        error: 1,
+        info: 2,
+        debug: 3,
+        verbose: 4
+    },
+    colors: {
+        warn: 'yellow',
+        error: 'red',
+        info: 'grey',
+        debug: 'blue',
+        verbose: 'green'
+    }
+};
+winston.addColors(customLoggingLevels.colors)
+
+const logger = winston.createLogger({
+    levels: customLoggingLevels.levels,
+    format: winston.format.combine(
+        winston.format.colorize({ all: true }),
+        winston.format.simple()
+    ),
+    transports: [
+        //
+        // - Write all logs with importance level of `error` or less to `error.log`
+        // - Write all logs with importance level of `info` or less to `combined.log`
+        //
+        new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
+        new winston.transports.File({ filename: 'logs/combined.log' }),
+    ],
+});
+
+//
+// If we're not in production then log to all the levels
+// else log only info, error and warn levels
+//
+if (process.env.NODE_ENV !== 'production') {
+    logger.add(new winston.transports.Console({ level: 'verbose' })); // Set the Console transport to log all levels from verbose and above
+} else {
+    logger.add(new winston.transports.Console({ level: 'info' })) // Set the Console transport to log all levels from info and above
+}
+
+module.exports = logger;
