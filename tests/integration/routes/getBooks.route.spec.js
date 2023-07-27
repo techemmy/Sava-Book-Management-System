e7a30c47310ec260a3dfd4bba01718c8d51fae7a
Emmanuel Oloyede <emmanueltopea@gmail.com>
2023-07-27 18:03:57 +0100

    test: write tests for getBooks route functionality

---
+++ tests/integration/routes/getBooks.route.spec.js
@@ -0,0 +1,27 @@
+const app = require("../../../src/app")
+const request = require("supertest")
+const redisStore = require("../../../src/redisStore");
+const testHelper = require("../../testHelper")
+const bookFixtures = require("../../bookFixtures.json")
+
+let redisClient;
+beforeAll(async () => {
+    redisClient = redisStore.getClient();
+    await redisClient.connect()
+})
+
+afterAll(async () => {
+    await testHelper.cleanupDb(redisClient)
+    await redisClient.disconnect();
+})
+
+describe("GET '/books' route", () => {
+    test("should get all the books in the database", async () => {
+        await testHelper.createBooks(bookFixtures, redisClient)
+        const response = await request(app).get("/books")
+        expect(response.status).toBe(200)
+        expect(response.body.status).toBeTruthy()
+        expect(response.body.books.length).toBe(bookFixtures.length)
+    })
+})
+
