e974617f634cf967e77ebf033d6c06e3b5f8a952
Emmanuel Oloyede <emmanueltopea@gmail.com>
2023-07-27 20:45:53 +0100

    test: write test for delete book by ISBN functionality route

---
+++ tests/integration/routes/deleteBookByISBN.spec.js
@@ -0,0 +1,33 @@
+const app = require("../../../src/app")
+const request = require("supertest")
+const redisStore = require("../../../src/redisStore");
+const testHelper = require("../../testHelper")
+const bookFixtures = require("../../bookFixtures.json")
+
+let redisClient;
+beforeAll(async () => {
+    redisClient = redisStore.getClient();
+    await redisClient.connect()
+    await testHelper.createBooks(bookFixtures, redisClient)
+
+})
+
+afterAll(async () => {
+    await testHelper.cleanupDb(redisClient)
+    await redisClient.disconnect();
+})
+
+describe("DELETE '/books' route", () => {
+    test("should delete a book", async () => {
+        const response = await request(app).delete(`/books/${bookFixtures[0].ISBN}`)
+        expect(response.status).toBe(200)
+        expect(response.body.status).toBeTruthy()
+    })
+
+    test("should return 404 error if book doesn't exist", async () => {
+        const response = await request(app).delete(`/books/${bookFixtures[0].ISBN}`)
+        expect(response.status).toBe(404)
+        expect(response.body.status).toBeFalsy()
+    })
+})
+
