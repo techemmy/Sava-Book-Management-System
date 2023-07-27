c7bb95ff6d8fa7560088450dc29935efb03b236f
Emmanuel Oloyede <emmanueltopea@gmail.com>
2023-07-27 18:49:59 +0100

    test: write tests for the search functionality route

---
+++ tests/integration/routes/searchBook.spec.js
@@ -0,0 +1,34 @@
+const app = require("../../../src/app")
+const request = require("supertest")
+const redisStore = require("../../../src/redisStore");
+const testHelper = require("../../testHelper")
+const bookFixtures = require("../../bookFixtures.json")
+
+let redisClient;
+beforeAll(async () => {
+    redisClient = redisStore.getClient();
+    await redisClient.connect()
+})
+
+afterAll(async () => {
+    await testHelper.cleanupDb(redisClient)
+    await redisClient.disconnect();
+})
+
+describe("GET '/books/search' route", () => {
+    test("should get search for books with 'l' in the database", async () => {
+        await testHelper.createBooks(bookFixtures, redisClient)
+        const searchResults = testHelper.searchTermInBooks('l', bookFixtures)
+        const response = await request(app).get("/books/search?term=l")
+        expect(response.status).toBe(200)
+        expect(response.body.status).toBeTruthy()
+        expect(response.body.books.length).toEqual(searchResults.length)
+    })
+
+    test("should fail to search database is no search term is found", async () => {
+        const response = await request(app).get("/books/search?")
+        expect(response.status).toBe(400)
+        expect(response.body.status).toBeFalsy()
+    })
+})
+
