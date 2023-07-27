3f6b82a18b5b4c6543a3b1405f8d378503f10883
Emmanuel Oloyede <emmanueltopea@gmail.com>
2023-07-27 15:36:56 +0100

    refactor: rename book to bookFixtures

--- tests/unit/bookModel.spec.js
+++ tests/unit/bookModel.spec.js
@@ -1,22 +1,21 @@
-const app = require("../../src/app")
 const bookModel = require("./../../src/bookModel")
-const books = require("./../fixtures.json")
+const bookFixtures = require("./../bookFixtures.json")
 
 describe("Book Model Tests", () => {
     it("should create book from the book model", async () => {
-        const book = new bookModel(books[0])
-        expect(book.title).toBe(books[0].title)
-        expect(book.author).toBe(books[0].author)
-        expect(book.ISBN).toBe(books[0].ISBN)
-        expect(book.publicationYear).toBe(books[0].publicationYear)
+        const book = new bookModel(bookFixtures[0])
+        expect(book.title).toBe(bookFixtures[0].title)
+        expect(book.author).toBe(bookFixtures[0].author)
+        expect(book.ISBN).toBe(bookFixtures[0].ISBN)
+        expect(book.publicationYear).toBe(bookFixtures[0].publicationYear)
     })
 
     it("should created book model should contain only book attributes", () => {
-        const book = new bookModel({...books[0], username: "John Doe"})
-        expect(book.title).toBe(books[0].title)
-        expect(book.author).toBe(books[0].author)
-        expect(book.ISBN).toBe(books[0].ISBN)
-        expect(book.publicationYear).toBe(books[0].publicationYear)
+        const book = new bookModel({...bookFixtures[0], username: "John Doe"})
+        expect(book.title).toBe(bookFixtures[0].title)
+        expect(book.author).toBe(bookFixtures[0].author)
+        expect(book.ISBN).toBe(bookFixtures[0].ISBN)
+        expect(book.publicationYear).toBe(bookFixtures[0].publicationYear)
         expect(book.username).toBeUndefined()
     })
 
@@ -25,11 +24,11 @@ describe("Book Model Tests", () => {
         expect(await book.validate()).toBeTruthy()
     })
 
-    it("should check if the method that checks for similarity in books work", () => {
-        const book = new bookModel(books[0])
-        expect(book.isSame(books[0])).toBeTruthy()
-        expect(book.isSame({...books[0], title: "Another"})).toBeFalsy()
-        expect(book.isSame(books[1])).toBeFalsy()
+    it("should check if the method that checks for similarity between books work", () => {
+        const book = new bookModel(bookFixtures[0])
+        expect(book.isSame(bookFixtures[0])).toBeTruthy()
+        expect(book.isSame({...bookFixtures[0], title: "Another"})).toBeFalsy()
+        expect(book.isSame(bookFixtures[1])).toBeFalsy()
     })
 
 })
