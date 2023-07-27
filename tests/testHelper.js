a9d286c215fb698a8a356e051ddf3c94dc88ea55
Emmanuel Oloyede <emmanueltopea@gmail.com>
2023-07-27 16:48:02 +0100

    test: add test helper to retrieve all hashes linked with a base key

--- tests/testHelper.js
+++ tests/testHelper.js
@@ -11,6 +11,25 @@ const deleteAllMatchingKeys = async (baseKey, redisClient) => {
     }
 }
 
+const getAllItemsMatchingKey = async (baseKey, redisClient) => {
+    /**
+     * Gets all the items matching the baseKey
+     * @param {string} baseKey - The key that will be used to get hashes
+     * @param {object} redisClient - A connected redis instance
+     * @returns {object} an array of items with matching key
+     */
+    const items = []
+    const keys = await redisClient.keys(`${baseKey}:*`)
+    for (const key of keys) {
+        const item = await redisClient.hGetAll(key);
+        items.push(item)
+    }
+
+    return items
+
+}
+
 module.exports = {
-    deleteAllMatchingKeys
+    deleteAllMatchingKeys,
+    getAllItemsMatchingKey
 }
